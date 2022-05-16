e9b785bcc914da345ccca365b81b5613d7eecede
rguezque <rguezque@gmail.com>
2022-05-16 16:16:53 +0000

    Deploying to gh-pages from @ rguezque/personal-portfolio@147f846f46080a31de48bd279c853003220ba375 🚀

--- _next/static/X22yi3f2e4cSTUFdNhdtE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a48fa246ea69397f.js"],"/404":["static/chunks/pages/404-5ac84bae197f5ede.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
